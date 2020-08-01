42607911c959311e7f7d837ce473d093de612341
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-08-01 03:53:07 +0200

    add @apollo/client and graphql

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { ApolloProvider } from '@apollo/client';
+import client from './config/apollo';
 
 export default function App() {
   return (
-    <div className="app">
-      <h1>Estamos en App</h1>
-      <Button primary>Primary</Button>
-      <Button secondary>Secondary</Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <h1>Estamos en App</h1>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
+    </ApolloProvider>
   );
 }
