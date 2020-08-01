ad8deee8eb0f89db285d95e04d34a821b574fdef
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2020-08-01 03:02:51 +0200

    add semantic-ui

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Crea un Instagram con React, GraphQL, Apollo, MongoDB y AWS
+
+_Curso en Udemy para crear un clone Instagram by Agustin Navarro Galdon_
+
+**Curso:** https://courses.agustinnavarrogaldon.com/instaclone
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'semantic-ui-react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Estamos en App</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
-
-export default App;
